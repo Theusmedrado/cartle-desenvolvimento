60b6f0ef32d97e94251273dcdf3f63a3b1890204
Theusmedrado <theusmedrado10@gmail.com>
2024-12-08 16:04:10 -0300

    Add files via upload

---
+++ home/home.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página</title>
+    <link rel="stylesheet" href="estilo.css"> <!-- Arquivo CSS -->
+</head>
+
+<body>
+    <!-- Cabeçalho -->
+    <header>
+        <div class="menu-container">
+            <button class="menu-toggle" onclick="toggleMenu()">Menu</button>
+           <div class="menu-options" id="menuOptions">
+                <a href="../login/login.php">Login</a>
+                <a href="../login/registro.php">Registrar</a>
+                <a href="#suporte">Suporte</a>
+
+            </div>
+           
+
+        </div>
+        <img src="imagens/iocn.png" alt="Logo">
+
+    </header>
+
+
+
+
+
+    <div class="video-background">
+        <video autoplay muted loop id="background-video">
+            <source src="imagens/8306452-uhd_4096_2160_25fps.mp4" type="video/mp4">
+
+        </video>
+    </div>
+
+    <div class="search-bar search">
+        <input type="text" placeholder="O que deseja buscar">
+        <button>Buscar</button>
+    </div>
+
+    <!-- Aba de Categorias -->
+    <div class="categories">
+        <ul>
+            <li><a href="#masculino">Masculino</a></li>
+            <li><a href="#feminino">Feminino</a></li>
+            <li><a href="#infantil">Infantil</a></li>
+            <li><a href="#esporte">Esporte</a></li>
+            <li><a href="#acessorios">Acessórios</a></li>
+        </ul>
+    </div>
+
+    <section class="listings destaque">
+        <h2>Destaques</h2>
+   
+        <div class="card">
+            <a href="..\produtos\pagina-produtos.php" class="link-produto">
+                <img src="imagens/calca-cargo.png" alt="" class="foto"></a>
+            <div class="info">
+                <h3>Calça cargo</h3>
+                <p>ShoppingBuy</p>
+                <p>R$150 </p>
+            </div>
+        </div>
+
+
+        <div class="card">
+            <a href="..\produtos\pagina-produtos.php" class="link-produto">
+                <img src="imagens/calca-cargo.png" alt="" class="foto"></a>
+            <div class="info">
+                <h3>Calça cargo</h3>
+                <p>ShoppingBuy</p>
+                <p>R$150 </p>
+            </div>
+        </div>
+
+
+        <div class="card">
+            <a href="..\produtos\pagina-produtos.php" class="link-produto">
+                <img src="imagens/calca-cargo.png" alt="" class="foto"></a>
+            <div class="info">
+                <h3>Calça cargo</h3>
+                <p>ShoppingBuy</p>
+                <p>R$150 </p>
+            </div>
+        </div>
+
+
+      
+
+    <!-- Fundo Escuro -->
+    <div class="overlay" id="overlay" onclick="closeAllModals()"></div>
+
+    <script src="scripit.js"></script> <!-- Arquivo JavaScript -->
+
+    <Footer>
+        <h2><a href="">Sobre Nós</a></h2>
+    </footer>
+
+
+</body>
+
+
+</html>
---
+++ login/editar-produto.php
@@ -0,0 +1,113 @@
+<?php
+require "../includes/cabecalho.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="..\home\estilo.css">
+    <title>Document</title>
+    
+    <style>
+        h1 {
+            text-align: center;
+            font-family: Arial, Helvetica, sans-serif;
+            margin-top: 5%;
+        }
+
+        .formulario {
+            background-color: #1d7373;
+            color: #fff;
+            text-align: center;
+            margin-left: 30%;
+            margin-right: 30%;
+            padding-bottom: 1%;
+            padding-top: 1%;
+            border-radius: 10px;
+        }
+        .adicionar{
+            display: flex;
+            border-bottom: solid 1px #fff;
+            text-align: center;
+            align-items: center;
+            padding-left: 10%;
+            justify-content: space-between;
+        }
+        P{
+            padding: 2%;
+            
+        }
+        img{
+            height: 30px;
+           padding-right: 5%;
+        }
+        @media  screen and (max-width:1600px) {
+            h1 {
+            text-align: center;
+            font-family: Arial, Helvetica, sans-serif;
+            margin-top: 5%;
+        }
+
+        .formulario {
+            background-color: #1d7373;
+            color: #fff;
+            text-align: center;
+            margin-left: 10%;
+            margin-right: 10%;
+            padding-bottom: 1%;
+            padding-top: 1%;
+            border-radius: 10px;
+        }
+        .adicionar{
+            display: flex;
+            border-bottom: solid 1px #fff;
+            text-align: center;
+            align-items: center;
+            padding-left: 10%;
+            justify-content: space-between;
+        }
+        P{
+            padding: 2%;
+            
+        }
+        img{
+            height: 30px;
+           padding-right: 5%;
+        }
+    }
+
+  
+    </style>
+</head>
+
+<body>
+    <section>
+        <h1>Adicionar ou remover produtos</h1>
+        <div class="formulario">
+        <div class="adicionar">
+            <p>calça cargo</p><img src="../home/imagens/8325947-mecanico-ferramenta-logotipo-icone-design-gratis-vetor.jpg" alt=""><img src="../home/imagens/excluir-o-botao-vermelho-do-icone-ilustracao-do-simbolo-de-lata-de-lixo_692379-615.avif" alt="">
+            </div>
+            <div class="adicionar">
+            <p>calça cargo</p><img src="../home/imagens/8325947-mecanico-ferramenta-logotipo-icone-design-gratis-vetor.jpg" alt=""><img src="../home/imagens/excluir-o-botao-vermelho-do-icone-ilustracao-do-simbolo-de-lata-de-lixo_692379-615.avif" alt="">
+            </div>
+          
+            <div class="adicionar">
+            <p>calça cargo</p><img src="../home/imagens/8325947-mecanico-ferramenta-logotipo-icone-design-gratis-vetor.jpg" alt=""><img src="../home/imagens/excluir-o-botao-vermelho-do-icone-ilustracao-do-simbolo-de-lata-de-lixo_692379-615.avif" alt="">
+            </div>
+            
+            <div class="adicionar">
+            <p>calça cargo</p><img src="../home/imagens/8325947-mecanico-ferramenta-logotipo-icone-design-gratis-vetor.jpg" alt=""><img src="../home/imagens/excluir-o-botao-vermelho-do-icone-ilustracao-do-simbolo-de-lata-de-lixo_692379-615.avif" alt="">
+            </div>
+          
+            <div class="adicionar">
+            <p>calça cargo</p><img src="../home/imagens/8325947-mecanico-ferramenta-logotipo-icone-design-gratis-vetor.jpg" alt=""><img src="../home/imagens/excluir-o-botao-vermelho-do-icone-ilustracao-do-simbolo-de-lata-de-lixo_692379-615.avif" alt="">
+            </div>
+        </div>
+    </section>
+
+
+</body>
+
+</html>
