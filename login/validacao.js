e2e12541b4ab6ba8c8041c0738d1cf597ee890cd
Theusmedrado <theusmedrado10@gmail.com>
2024-12-09 11:49:57 -0300

    Delete login directory

--- login/validacao.js
+++
@@ -1,9 +0,0 @@
-document.querySelector("form").addEventListener("submit", function(event) {
-    var senha = document.querySelector("input[name='password']").value;
-    var confirmarSenha = document.querySelector("input[name='nova-password']").value;
-
-    if (senha !== confirmarSenha) {
-        event.preventDefault();  
-        alert("As senhas não coincidem. Por favor, verifique.");
-    }
-});
