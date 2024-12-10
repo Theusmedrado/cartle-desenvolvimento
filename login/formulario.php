706b0180bcd2fe64da1b4fb077e5d5accd36185e
Theusmedrado <theusmedrado10@gmail.com>
2024-12-10 17:32:53 -0300

    Update formulario.php

--- login/formulario.php
+++ login/formulario.php
@@ -1,11 +1,16 @@
 <?php 
-require "../includes/cabecalho.php"
-/*require "../includes/conecta.php";
+require "../includes/cabecalho.php";
+require "../includes/conecta.php";
 require "../login/funcoes-produto.php";
+// mysqli, $produto, $imagem, $categoria, $tipo, $descricao, $data, $valor
 if(isset($_POST['registrar'])){
-	$produto = $_POST['titulo'];
-	$texto = $_POST['texto'];
-	$resumo = $_POST['resumo'];
+	$produto = $_POST['produto'];
+	$imagem = $_FILES['imagem'];
+	$categoria = $_POST['categoria'];
+	$tipo = $_POST['tipo'];
+	$descricao = $_POST['descricao'];
+    $data = $_POST['data'];
+    $valor = $_POST['valor'];
 
 	// Capturando os dados da imagem que será enviada
 	$imagem = $_FILES['imagem']; // Isso acessa o campo imagem do form.
@@ -14,12 +19,12 @@ if(isset($_POST['registrar'])){
 	upload($imagem);
 
 	// Enviar os dados para o banco de dados
-	inserirNoticia(
-		$conexao, $titulo, $texto, $resumo, $imagem['name'], $usuarioId);
+	inserirproduto(
+		$mysqli, $produto, $imagem['name'],$categoria,$tipo,$descricao,$data, $valor);
 
 	// Redirecionando para a página de noticias.php
-	header("location:noticias.php");
-}*/
+	header("location: editar-produto.php");
+}
 
 ?>
 
@@ -75,23 +80,23 @@ if(isset($_POST['registrar'])){
 <body>
     <section>
         <h1>Registro de Produtos</h1>
-        <form action="" autocomplete="off" enctype="multipart/form-data" method="post" name="inserir-formulario">
+        <form action="" autocomplete="off" enctype="multipart/form-data" method="post" name="inserir">
             <div class="enviar">
-            <label class="label">nome do produto:</label ><input type="text" name="produto-formulario" class="produto formulario">
+            <label class="label">nome do produto:</label ><input type="text" name="produto" class="produto formulario">
             <br>
-            <label class="label">imagem:</label><input type="file" name="imagem-formulario" class="imagem formulario">
+            <label class="label">imagem:</label><input type="file" name="imagem" class="imagem formulario">
             <br>
-            <label class="label">categoria:</label><input type="text" name="categoria-formulario" class="categoria formulario">
+            <label class="label">categoria:</label><input type="text" name="categoria" class="categoria formulario">
             <br>
-            <label class="label">tipo:</label><input type="text" name="tipo-formulario" class="tipo formulario">
+            <label class="label">tipo:</label><input type="text" name="tipo" class="tipo formulario">
             <br>
-            <label class="label">data</label><input type="date" name="data-formulario" class="data formulario">
+            <label class="label">data</label><input type="date" name="data" class="data formulario">
             <br>
-            <label class="label">descrição:</label><textarea type="text" name="descricao-formulario" placeholder="máximo 400 caracteres.." class="descricao formulario"></textarea>
+            <label class="label">descrição:</label><textarea type="text" name="descricao" placeholder="máximo 400 caracteres.." class="descricao formulario"></textarea>
             <br>
-            <label class="label">valor:</label><input type="number" name="valor-formulario" class="valor formulario">
+            <label class="label">valor:</label><input type="number" name="valor" class="valor formulario" step="0.01">
             <br>
-            <input type="submit" value="registrar" class="botao formulario" >
+            <button class="botao formulario" name="registrar" >registrar</button>
         </div>
         </form>
     </section>
