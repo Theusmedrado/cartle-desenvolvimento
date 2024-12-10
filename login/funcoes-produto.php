ecff1e27c86cc4111dac374a1eaa11fac4c28b5d
Theusmedrado <theusmedrado10@gmail.com>
2024-12-10 17:33:43 -0300

    Update funcoes-produto.php

--- login/funcoes-produto.php
+++ login/funcoes-produto.php
@@ -5,10 +5,48 @@ require "../includes/conecta.php";
 function inserirproduto($mysqli, $produto, $imagem, $categoria, $tipo, $descricao, $data, $valor)
 {
 
-    $conexao = "INSERT INTO produtos(produto,imagem,categoria,tipo,descricao,dia,valor)
+    $conexao = "INSERT INTO produtos(produto,imagem,categoria,tipo,descricao,data,valor)
             VALUES('$produto', '$imagem', '$categoria', '$tipo' ,'$descricao', '$data' , '$valor')";
 
 
-    executarQuery($mysqli, $conexao);
+    executarQuery ($mysqli, $conexao);
+}
+function executarQuery($mysqli, $conexao)
+{
+    $consulta = mysqli_query($mysqli, $conexao) or die(mysqli_error($mysqli));
+    return $consulta;
+}
+function upload($arquivo){
+
+    /* Array para validação dos tipos permitidos */
+    $tiposValidos = [
+        "image/png",
+        "image/jpeg",
+        "image/gif",
+        "image/svg+xml"
+    ];
+    /* Verificando se o tipo do arquivo NÃO É 
+    um dos existentes no array tiposValidos */
+    if (!in_array($arquivo['type'], $tiposValidos)) {
+        // Se não for para tudo, dá um alerta e volta pra página anterior
+        echo "<script> 
+                alert('Formato inválido!');
+                history.back();
+            </script>";
+        exit; // mesma coisa que o die()
+        /* Pegando apenas o nome/extensão do arquivo */
+    $nome = $arquivo['name'];
+
+    /* Obtendo do servidor o local/nome temporário
+    para o processo de upload */
+    $temporario = $arquivo['tmp_name'];
+
+    /* Definindo da pasta de destino + nome do arquivo da imagem */
+    $destino = "../imagens/" . $nome;
+
+    /* Movendo o arquivo/imagem da área temporária
+    para a pasta de destino indicada (imagens) */
+    move_uploaded_file($temporario, $destino);
+}
 }
 ?>
